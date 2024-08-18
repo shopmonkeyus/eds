375c38c3db7e7817ab040919a78e761f38d96cb9
Jeff Haynie <jhaynie@shopmonkey.io>
2024-08-17 22:25:22 -0500

    [VCARB-50] unit tests

---
+++ internal/consumer/consumer_test.go
@@ -0,0 +1,414 @@
+package consumer
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	natsserver "github.com/nats-io/nats-server/v2/test"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"github.com/shopmonkeyus/eds-server/internal"
+	"github.com/shopmonkeyus/eds-server/internal/util"
+	"github.com/shopmonkeyus/go-common/logger"
+	"github.com/stretchr/testify/assert"
+	"github.com/vmihailenco/msgpack"
+)
+
+func runNatsTestServer(fn func(natsurl string, nc *nats.Conn, js jetstream.JetStream)) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	port := l.Addr().(*net.TCPAddr).Port
+	opts := natsserver.DefaultTestOptions
+	opts.Port = port
+	opts.Cluster.Name = "testing"
+	opts.JetStream = true
+	l.Close()
+	srv := natsserver.RunServer(&opts)
+	defer srv.Shutdown()
+	url := fmt.Sprintf("nats://localhost:%d", port)
+	nc, err := nats.Connect(url)
+	if err != nil {
+		panic(err)
+	}
+	defer nc.Close()
+	js, err := jetstream.New(nc)
+	if err != nil {
+		panic(err)
+	}
+	if _, err := js.CreateStream(context.Background(), jetstream.StreamConfig{
+		Name:         "dbchange",
+		Subjects:     []string{"dbchange.>"},
+		MaxConsumers: 1,
+		Storage:      jetstream.MemoryStorage,
+	}); err != nil {
+		panic(err)
+	}
+	fn(url, nc, js)
+}
+
+type mockDriver struct {
+	maxBatchSize int
+	flush        func(logger logger.Logger) error
+	process      func(logger logger.Logger, event internal.DBChangeEvent) (bool, error)
+}
+
+func (m *mockDriver) Flush(logger logger.Logger) error {
+	if m.flush != nil {
+		return m.flush(logger)
+	}
+	return nil
+}
+func (m *mockDriver) Process(logger logger.Logger, event internal.DBChangeEvent) (bool, error) {
+	if m.process != nil {
+		return m.process(logger, event)
+	}
+	return false, nil
+}
+
+func (m *mockDriver) MaxBatchSize() int {
+	return m.maxBatchSize
+}
+
+func TestStartStop(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+		mockDriver := &mockDriver{}
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context: context.Background(),
+			Logger:  logger.NewTestLogger(),
+			Driver:  mockDriver,
+			URL:     natsurl,
+		})
+		assert.NoError(t, err)
+		assert.NoError(t, consumer.Stop())
+	})
+}
+
+func TestSingleMessage(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+		var testEvent *internal.DBChangeEvent
+		var flushed bool
+
+		mockDriver := &mockDriver{
+			process: func(logger logger.Logger, event internal.DBChangeEvent) (bool, error) {
+				testEvent = &event
+				return false, nil
+			},
+			flush: func(logger logger.Logger) error {
+				flushed = true
+				return nil
+			},
+		}
+
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context: context.Background(),
+			Logger:  logger.NewTestLogger(),
+			Driver:  mockDriver,
+			URL:     natsurl,
+		})
+
+		assert.NoError(t, err)
+
+		var sendEvent internal.DBChangeEvent
+		sendEvent.Table = "order"
+		sendEvent.Operation = "INSERT"
+		sendEvent.Timestamp = time.Now().UnixMilli()
+		sendEvent.MVCCTimestamp = fmt.Sprintf("%v", time.Now().UnixNano())
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.LID.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		time.Sleep(time.Millisecond * 100)
+
+		assert.NoError(t, consumer.Stop())
+		assert.NotNil(t, testEvent)
+		assert.True(t, flushed)
+
+		assert.Equal(t, sendEvent.Table, testEvent.Table)
+		assert.Equal(t, sendEvent.Operation, testEvent.Operation)
+		assert.Equal(t, sendEvent.Timestamp, testEvent.Timestamp)
+		assert.Equal(t, sendEvent.MVCCTimestamp, testEvent.MVCCTimestamp)
+	})
+}
+
+func TestSingleMessageWithFlush(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+
+		var testEvent *internal.DBChangeEvent
+		var flushed bool
+
+		mockDriver := &mockDriver{
+			maxBatchSize: 2,
+			process: func(logger logger.Logger, event internal.DBChangeEvent) (bool, error) {
+				testEvent = &event
+				return true, nil
+			},
+			flush: func(logger logger.Logger) error {
+				flushed = true
+				return nil
+			},
+		}
+
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context: context.Background(),
+			Logger:  logger.NewTestLogger(),
+			Driver:  mockDriver,
+			URL:     natsurl,
+		})
+
+		assert.NoError(t, err)
+
+		var sendEvent internal.DBChangeEvent
+		sendEvent.Table = "order"
+		sendEvent.Operation = "INSERT"
+		sendEvent.Timestamp = time.Now().UnixMilli()
+		sendEvent.MVCCTimestamp = fmt.Sprintf("%v", time.Now().UnixNano())
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.LID.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		time.Sleep(time.Millisecond * 100)
+
+		assert.NotNil(t, testEvent)
+		assert.True(t, flushed)
+		assert.Equal(t, sendEvent.Table, testEvent.Table)
+		assert.Equal(t, sendEvent.Operation, testEvent.Operation)
+		assert.Equal(t, sendEvent.Timestamp, testEvent.Timestamp)
+		assert.Equal(t, sendEvent.MVCCTimestamp, testEvent.MVCCTimestamp)
+
+		assert.NoError(t, consumer.Stop())
+	})
+}
+
+func TestMultipleMessagesWithFlushAndMaxBatchSize(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+
+		var testEvents []internal.DBChangeEvent
+		var flushed int
+
+		mockDriver := &mockDriver{
+			maxBatchSize: 2,
+			process: func(logger logger.Logger, event internal.DBChangeEvent) (bool, error) {
+				testEvents = append(testEvents, event)
+				return false, nil
+			},
+			flush: func(logger logger.Logger) error {
+				flushed++
+				return nil
+			},
+		}
+
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context: context.Background(),
+			Logger:  logger.NewTestLogger(),
+			Driver:  mockDriver,
+			URL:     natsurl,
+		})
+
+		assert.NoError(t, err)
+
+		var sendEvent internal.DBChangeEvent
+		sendEvent.Table = "order"
+		sendEvent.Operation = "INSERT"
+		sendEvent.Timestamp = time.Now().UnixMilli()
+		sendEvent.MVCCTimestamp = fmt.Sprintf("%v", time.Now().UnixNano())
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.1.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.2.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		time.Sleep(time.Millisecond * 100)
+
+		assert.Len(t, testEvents, 2)
+		assert.Equal(t, 1, flushed)
+
+		assert.NoError(t, consumer.Stop())
+	})
+}
+
+func TestMultipleMessagesWithFlushUsingProcess(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+
+		var testEvents []internal.DBChangeEvent
+		var flushed int
+
+		mockDriver := &mockDriver{
+			maxBatchSize: -1,
+			process: func(logger logger.Logger, event internal.DBChangeEvent) (bool, error) {
+				testEvents = append(testEvents, event)
+				return len(testEvents) == 2, nil
+			},
+			flush: func(logger logger.Logger) error {
+				flushed++
+				return nil
+			},
+		}
+
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context: context.Background(),
+			Logger:  logger.NewTestLogger(),
+			Driver:  mockDriver,
+			URL:     natsurl,
+		})
+
+		assert.NoError(t, err)
+
+		var sendEvent internal.DBChangeEvent
+		sendEvent.Table = "order"
+		sendEvent.Operation = "INSERT"
+		sendEvent.Timestamp = time.Now().UnixMilli()
+		sendEvent.MVCCTimestamp = fmt.Sprintf("%v", time.Now().UnixNano())
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.1.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.2.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		time.Sleep(time.Millisecond * 100)
+
+		assert.Len(t, testEvents, 2)
+		assert.Equal(t, 1, flushed)
+
+		assert.NoError(t, consumer.Stop())
+	})
+}
+
+func TestMultipleMessagesWithMaxAckPending(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+
+		var testEvents []internal.DBChangeEvent
+		var flushed int
+
+		max := 100
+
+		mockDriver := &mockDriver{
+			maxBatchSize: -1,
+			process: func(logger logger.Logger, event internal.DBChangeEvent) (bool, error) {
+				testEvents = append(testEvents, event)
+				return false, nil
+			},
+			flush: func(logger logger.Logger) error {
+				flushed++
+				return nil
+			},
+		}
+
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context:       context.Background(),
+			Logger:        logger.NewTestLogger(),
+			Driver:        mockDriver,
+			URL:           natsurl,
+			MaxAckPending: max,
+		})
+
+		assert.NoError(t, err)
+
+		var sendEvent internal.DBChangeEvent
+		sendEvent.Table = "order"
+		sendEvent.Operation = "INSERT"
+		sendEvent.Timestamp = time.Now().UnixMilli()
+		sendEvent.MVCCTimestamp = fmt.Sprintf("%v", time.Now().UnixNano())
+
+		for i := 0; i < max; i++ {
+			_, err = js.Publish(context.Background(), fmt.Sprintf("dbchange.order.INSERT.CID.%d.PUBLIC.1", i+1), []byte(util.JSONStringify(sendEvent)))
+			assert.NoError(t, err)
+		}
+
+		time.Sleep(time.Millisecond * 100)
+
+		assert.Len(t, testEvents, max)
+		assert.Equal(t, 1, flushed)
+
+		assert.NoError(t, consumer.Stop())
+	})
+}
+
+func TestHeartbeats(t *testing.T) {
+	runNatsTestServer(func(natsurl string, nc *nats.Conn, js jetstream.JetStream) {
+
+		internal.MetricsReset() // force a collection to reset
+
+		mockDriver := &mockDriver{}
+
+		var received []*nats.Msg
+		var subscription *nats.Subscription
+
+		consumer, err := NewConsumer(ConsumerConfig{
+			Context:           context.Background(),
+			Logger:            logger.NewTestLogger(),
+			Driver:            mockDriver,
+			URL:               natsurl,
+			HeartbeatInterval: time.Second,
+			sessionIDCallback: func(id string) {
+				subscription, _ = nc.Subscribe("eds.client."+id+".heartbeat", func(msg *nats.Msg) {
+					received = append(received, msg)
+				})
+			},
+		})
+
+		assert.NoError(t, err)
+
+		time.Sleep(time.Millisecond * 150)
+
+		assert.Len(t, received, 1)
+
+		msg := received[0]
+
+		assert.Equal(t, "msgpack", msg.Header.Get("content-encoding"))
+
+		var payload heartbeat
+		assert.NoError(t, msgpack.NewDecoder(bytes.NewReader(msg.Data)).UseJSONTag(true).Decode(&payload))
+
+		assert.NotEmpty(t, payload.SessionId)
+		assert.Equal(t, consumer.sessionID, payload.SessionId)
+		assert.Equal(t, int64(0), payload.Offset)
+		assert.Equal(t, time.Duration(0), payload.Uptime)
+		assert.Equal(t, float64(0), payload.Stats.Metrics.TotalEvents)
+		assert.Equal(t, float64(0), payload.Stats.Metrics.FlushCount)
+		assert.Equal(t, float64(0), payload.Stats.Metrics.FlushDuration)
+		assert.Equal(t, float64(0), payload.Stats.Metrics.ProcessingDuration)
+		assert.Equal(t, float64(0), payload.Stats.Metrics.PendingEvents)
+
+		internal.MetricsReset() // force a collection to reset
+
+		var sendEvent internal.DBChangeEvent
+		sendEvent.Table = "order"
+		sendEvent.Operation = "INSERT"
+		sendEvent.Timestamp = time.Now().UnixMilli()
+		sendEvent.MVCCTimestamp = fmt.Sprintf("%v", time.Now().UnixNano())
+
+		_, err = js.Publish(context.Background(), "dbchange.order.INSERT.CID.1.PUBLIC.1", []byte(util.JSONStringify(sendEvent)))
+		assert.NoError(t, err)
+
+		time.Sleep(time.Second + time.Millisecond*100)
+
+		assert.Len(t, received, 2)
+
+		subscription.Drain()
+		subscription.Unsubscribe()
+
+		assert.NoError(t, consumer.Stop())
+
+		msg = received[1]
+		var payload2 heartbeat
+		assert.Equal(t, "msgpack", msg.Header.Get("content-encoding"))
+		assert.NoError(t, msgpack.NewDecoder(bytes.NewReader(msg.Data)).UseJSONTag(true).Decode(&payload2))
+		assert.NotEmpty(t, payload2.SessionId)
+		assert.Equal(t, consumer.sessionID, payload2.SessionId)
+		assert.Equal(t, int64(1), payload2.Offset)
+		assert.Greater(t, payload2.Uptime, time.Duration(0))
+		assert.Equal(t, float64(1), payload2.Stats.Metrics.TotalEvents)
+		assert.Equal(t, float64(0), payload2.Stats.Metrics.FlushCount)
+		assert.Equal(t, float64(0), payload2.Stats.Metrics.FlushDuration)
+		assert.Equal(t, float64(0), payload2.Stats.Metrics.ProcessingDuration)
+		assert.Equal(t, float64(0), payload2.Stats.Metrics.PendingEvents)
+	})
+}
