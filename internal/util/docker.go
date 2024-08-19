23feb2bb67fde396215e034a603f394324242226
Bobby Christopher <bchristopher@shopmonkey.io>
2024-08-19 11:55:51 -0400

    removed prints

--- internal/util/docker.go
+++ internal/util/docker.go
@@ -1,18 +1,15 @@
 package util
 
 import (
-	"fmt"
 	"os"
 	"strings"
 )
 
 // IsRunningInsideDocker returns true if the process is running inside a docker container.
 func IsRunningInsideDocker() bool {
-	fmt.Println("IsRunningInsideDocker 1")
 	if Exists("/.dockerenv") {
 		return true
 	}
-	fmt.Println("IsRunningInsideDocker 2")
 
 	if Exists("/proc/1/cgroup") {
 		buf, _ := os.ReadFile("/proc/1/cgroup")
@@ -21,7 +18,5 @@ func IsRunningInsideDocker() bool {
 			return strings.Contains(contents, "docker") || strings.Contains(contents, "lxc") || strings.Contains(contents, "rt")
 		}
 	}
-	fmt.Println("IsRunningInsideDocker 3")
-
 	return false
 }
