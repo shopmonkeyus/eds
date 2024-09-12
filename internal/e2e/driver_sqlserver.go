4005a76683be64699fc4c1aedaaefc4542d1a636
Jeff Haynie <jhaynie@shopmonkey.io>
2024-09-12 12:28:52 -0500

    use master as sqlserver db name

--- internal/e2e/driver_sqlserver.go
+++ internal/e2e/driver_sqlserver.go
@@ -22,12 +22,12 @@ func (d *driverSQLServerTest) Name() string {
 }
 
 func (d *driverSQLServerTest) URL(dir string) string {
-	return fmt.Sprintf("sqlserver://sa:%s@127.0.0.1:1433/eds", dbpass)
+	return fmt.Sprintf("sqlserver://sa:%s@127.0.0.1:1433/master", dbpass)
 }
 
 func (d *driverSQLServerTest) Test(logger logger.Logger, dir string, nc *nats.Conn, js jetstream.JetStream, url string) error {
 	return runTest(logger, nc, js, func(event internal.DBChangeEvent) internal.DBChangeEvent {
-		return validateSQLEvent(logger, event, "sqlserver", fmt.Sprintf("sqlserver://sa:%s@127.0.0.1:1433?&database=eds&encrypt=disable", dbpass), func(table string) string {
+		return validateSQLEvent(logger, event, "sqlserver", fmt.Sprintf("sqlserver://sa:%s@127.0.0.1:1433?&database=master&encrypt=disable", dbpass), func(table string) string {
 			return fmt.Sprintf("`%s`", table)
 		}, "?")
 	})
