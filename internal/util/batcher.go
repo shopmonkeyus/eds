805d1e1281d662f553583084796c075fc97e33cc
Jerome Davenport <jdavenport@shopmonkey.io>
2025-07-14 14:58:14 -0500

    refactor batcher add

--- internal/util/batcher.go
+++ internal/util/batcher.go
@@ -42,45 +42,71 @@ func (b *Batcher) Add(table string, id string, operation string, diff []string, 
 		}
 	}
 	hashkey := table + primaryKey
-	index, found := b.pks[hashkey]
+	index, previousEventFound := b.pks[hashkey]
 
 	appendRecord := func() {
 		b.records = append(b.records, &Record{Table: table, Id: primaryKey, Operation: operation, Diff: diff, Object: payload, Event: event})
 		b.pks[hashkey] = uint(len(b.records) - 1)
 	}
 
-	if operation == "DELETE" {
-		if found {
-			// if found, remove the old record (could be insert or update)
-			b.records = append(b.records[:index], b.records[index+1:]...)
-			delete(b.pks, hashkey)
-		}
+	appendDeleteRecord := func() {
 		b.records = append(b.records, &Record{Table: table, Id: primaryKey, Operation: operation, Event: event})
 		b.pks[hashkey] = uint(len(b.records) - 1)
-	} else {
-		if found {
-			entry := b.records[index]
-			if entry.Operation == "INSERT" {
-				// don't combine if previous entry was an insert
-				appendRecord()
-			} else {
-				//For UPDATE operations, merge the diff keys
-				for _, key := range diff {
-					if !SliceContains(entry.Diff, key) {
-						entry.Diff = append(entry.Diff, key)
-					}
-				}
-				if len(entry.Diff) == 0 {
-					entry.Diff = diff
-				}
-				entry.Event = event
-				entry.Operation = operation
-				maps.Copy(entry.Object, payload) // upsert the payload with the new update
-			}
+	}
+
+	type batchCondition int
+	const (
+		withoutBatch batchCondition = iota
+		updateWithBatch
+		deleteWithBatch
+		deleteWithoutBatch
+	)
+
+	var previousRecord *Record
+	if previousEventFound {
+		previousRecord = b.records[index]
+	}
+
+	batchType := withoutBatch
+	switch operation {
+	case "DELETE":
+		if previousEventFound {
+			batchType = deleteWithBatch
 		} else {
-			// not found just add it
-			appendRecord()
+			batchType = deleteWithoutBatch
 		}
+	case "UPDATE":
+		if previousEventFound {
+			switch previousRecord.Operation {
+			case "INSERT":
+				batchType = withoutBatch
+			case "UPDATE":
+				batchType = updateWithBatch
+			}
+		}
+	}
+
+	switch batchType {
+	case withoutBatch:
+		appendRecord()
+	case updateWithBatch:
+		for _, key := range diff {
+			if !SliceContains(previousRecord.Diff, key) {
+				previousRecord.Diff = append(previousRecord.Diff, key)
+			}
+		}
+		if len(previousRecord.Diff) == 0 {
+			previousRecord.Diff = diff
+		}
+		previousRecord.Event = event
+		previousRecord.Operation = operation
+		maps.Copy(previousRecord.Object, payload) // upsert the payload with the new update
+	case deleteWithBatch:
+		b.records = append(b.records[:index], b.records[index+1:]...)
+		delete(b.pks, hashkey)
+		appendDeleteRecord()
+	case deleteWithoutBatch:
+		appendDeleteRecord()
 	}
 }
 
