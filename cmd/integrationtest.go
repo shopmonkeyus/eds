6de8a3cf3d17000608e63cfd1719e823b58e6e53
Jerry Davenport <jdavenport@shopmonkey.io>
2025-10-16 12:39:58 -0500

    [LOTUS-6496] Add file data integration test to EDS (#498)
    * add file data test
    
    * add file data test

--- cmd/integrationtest.go
+++ cmd/integrationtest.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/shopmonkeyus/eds/internal/integrationtest"
 	"github.com/shopmonkeyus/go-common/logger"
 	"github.com/spf13/cobra"
@@ -9,12 +11,14 @@ import (
 func RunWithLogAndRecover(fn func(cmd *cobra.Command, args []string, log logger.Logger)) func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		log := newLogger(cmd)
+		log.Info(fmt.Sprintf("starting integration test: %s", cmd.Name()))
 		defer func() {
 			if err := recover(); err != nil {
 				log.Error("error running integration test: %s", err)
 			}
 		}()
 		fn(cmd, args, log)
+		log.Info(fmt.Sprintf("completed integration test: %s", cmd.Name()))
 	}
 }
 
@@ -32,22 +36,41 @@ var loadTestRandomCmd = &cobra.Command{
 	Short: "Send random messages to nats",
 	Long:  "Send random messages to nats for integration testing. Currently sends customer update messages",
 	Run: RunWithLogAndRecover(func(cmd *cobra.Command, args []string, log logger.Logger) {
-		log.Info("Starting load-random integration test")
 		count, _ := cmd.Flags().GetInt("count")
 
 		js := integrationtest.NewConnection(&integrationtest.JetstreamConnection{})
 
-		log.Info("Sending %d customer messages", count)
+		log.Info("sending %d customer messages", count)
 
 		delivered := integrationtest.PublishRandomMessages(js, count, log)
 
-		log.Info("Completed sending %d customer messages to NATS", delivered)
+		log.Info("sent %d customer messages to NATS", delivered)
+	}),
+}
+
+var publishFileDataCmd = &cobra.Command{
+	Use:   "publish-file-data",
+	Short: "Publish test data to NATS from file",
+	Long:  "Publishes dbchange events from JSON file to NATS\nTest data should be at path '..eds_test_data/output.jsonl.tar.gz'\n@jdavenport for details",
+	Run: RunWithLogAndRecover(func(cmd *cobra.Command, args []string, log logger.Logger) {
+		count, _ := cmd.Flags().GetInt("count")
+
+		js := integrationtest.NewConnection(&integrationtest.JetstreamConnection{})
+
+		path := "../eds_test_data/output.jsonl.tar.gz"
+		delivered := integrationtest.PublishTestData(path, count, js, log)
+
+		log.Info("sent %d messages", delivered)
 	}),
 }
 
 func init() {
 	loadTestRandomCmd.Flags().Int("count", 1, "Number of customer messages to send (default: 1)")
 
+	publishFileDataCmd.Flags().Int("count", 1, "Number of messages to send (default: 1)")
+
 	integrationtestCmd.AddCommand(loadTestRandomCmd)
+	integrationtestCmd.AddCommand(publishFileDataCmd)
+
 	rootCmd.AddCommand(integrationtestCmd)
 }
