f25e02a42c618d3d912f323ea73fd517be6dd4e1
Cory Lewis <clewis@shopmonkey.io>
2026-02-02 23:56:45 -0800

    add updatedDate to schemas

--- internal/e2e/api.go
+++ internal/e2e/api.go
@@ -83,6 +83,10 @@ func setupServer(logger logger.Logger, creds string) (int, ShutdownFunc) {
 					"age": {
 						Type: "number",
 					},
+					"updatedDate": {
+						Type:   "string",
+						Format: "date-time",
+					},
 				},
 				PrimaryKeys: []string{"id"},
 			}
@@ -117,6 +121,10 @@ func setupServer(logger logger.Logger, creds string) (int, ShutdownFunc) {
 				},
 				"name": {
 					Type: "string",
+				},
+				"updatedDate": {
+					Type:   "string",
+					Format: "date-time",
 				},
 			},
 			PrimaryKeys: []string{"id"},
